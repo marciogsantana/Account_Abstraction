69706a560e759461e1665c65987430fdb0a5d5e8
Márcio Gomes de Santana <68255679+marciogsantana@users.noreply.github.com>
2023-05-24 14:00:31 -0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.12",
   networks: {
-    goerli: {
+    mumbai: {
       url: process.env.STAGING_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
